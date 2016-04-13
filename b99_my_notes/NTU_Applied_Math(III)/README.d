0c0046481fe457aec21495e7446d91009fcaf19f
wchuanghard <wchuang@g.harvard.edu>
2016-04-12 20:07:14 -0700

    my old notes, exams, psets, and whatnots

--- b99_my_notes/NTU_Applied_Math(III)/README.d
+++ b99_my_notes/NTU_Applied_Math(III)/README.d
@@ -1,5 +1,6 @@
 -Each week we had two lectures, and one homework (problem) set.
 (This repository has some remnants/heritages of them.)
 
--We had roughly 1.5 hours for the midterm and final exams.
-(Both were closed book exams!)
+-We had roughly 1.5 hours for the midterm and final exams; that is, it's less than 10 minutes for each problem.
+
+Mind that both were closed book exams!
