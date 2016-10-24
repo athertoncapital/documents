f10cae0478cbc0184f63f8c7400f9ac5c25844ef
William_Chuang <whchuang@usfca.edu>
2016-10-23 22:35:03 -0700

    Update README.d

--- b99_my_notes/NTU_Applied_Math(III)/README.d
+++ b99_my_notes/NTU_Applied_Math(III)/README.d
@@ -1,5 +1,5 @@
--Each week we had two lectures, and one homework (problem) set.
+##Each week we had two lectures, and one homework (problem) set.
 (This repository has some remnants/heritages of them.)
 
--We had roughly 1.5 hours for the midterm and final exams; that is, it's less than 10 minutes for each problem.
+##We had roughly 1.5 hours for the midterm and final exams; that is, it's less than 10 minutes for each problem.
 Mind that both were closed-book exams.
